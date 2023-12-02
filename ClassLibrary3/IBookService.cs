729b263bcaf335d9c8b87fb529ba9f3c16b5df98
nikhil9192 <nikhilsinghtech2580@gmail.com>
2023-12-02 09:27:16 +0530

    Add project files.

---
+++ ClassLibrary3/IBookService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary3
+{
+
+    public interface IBookService
+    {
+        IEnumerable<string> GetAllBooksForCategory(string categoryId);
+        IEnumerable<string> GetBooksForCategory(string categoryid);
+    }
+    public interface IEmailSender
+    {
+        void SendEmail(string to, string subject, string body);
+    }
+    public  class AccountService
+    {
+
+        private IBookService _bookService;
+        public AccountService(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+        public IEnumerable<string> GetAllBooksForCategory(string categoryId)
+        {
+            var allBooks = _bookService.GetBooksForCategory(categoryId);//fake
+            return allBooks;
+        }
+    }
+}
---
+++ MoqDemo/LibraryTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework.Legacy;
+using NUnit.Framework;
+using ClassLibrary3;
+
+namespace MoqDemo
+{
+    [TestFixture]
+    public class LibraryTest
+    {
+        [Test]
+        public void GetAllBooksForCategory_returns_list_of_available_books()
+        {
+            //1
+            var bookServiceStub = new Mock<IBookService>();
+            //2
+            bookServiceStub
+                .Setup(x => x.GetBooksForCategory("UnitTesting"))
+                .Returns(new List<string>
+                {
+            "The Art of Unit Testing",
+            "Test-Driven Development",
+            "Working Effectively with Legacy Code"
+                });
+            //3
+            var accountService = new AccountService(bookServiceStub.Object);
+            IEnumerable<string> result = accountService.GetAllBooksForCategory("UnitTesting");
+            ClassicAssert.AreEqual(3, result.Count());
+        }
+
+
+    }
+    }
